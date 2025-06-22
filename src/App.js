23909c9c5551caf710759a446411cb21d5eb4ff5
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2025-06-22 13:25:55 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import logo from "./logo-shecodes.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        {/* -05:27 */}
+      </header>
+    </div>
+  );
+}
