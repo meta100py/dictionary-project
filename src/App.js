e4d1ece6b8b36726f4e2c3fc84ad6c43a971aeb3
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2025-06-25 10:01:46 -0300

    Created the search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import logo from "./logo-shecodes.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        {/* -05:27 */}
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Valen</small>
+        </footer>
+      </div>
     </div>
   );
 }
