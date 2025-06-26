e81dfa51b6f8b0c8b1f2540c2232437242721fab
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2025-06-26 18:13:35 -0300

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data.meanings[0].definition);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+    let apiKey = "bdbe141f0f305d4eao50fcf7t80ff6bb";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
