e6f0909d30c7a864752d3b0b5b5f9bee2469059a
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2025-06-28 23:55:19 -0300

    Rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-
+  let [results, setResults] = useState(null);
   function handleResponse(response) {
-    console.log(response.data.meanings[0].definition);
+    // console.log(response);
+    setResults(response.data);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
     // documentation: https://www.shecodes.io/learn/apis/dictionary
     let apiKey = "bdbe141f0f305d4eao50fcf7t80ff6bb";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
@@ -26,6 +27,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
