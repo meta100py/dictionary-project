7a01ab7c71cb08dfa32f27a50061c34707b1f88d
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2025-07-01 10:21:27 -0300

    Added a link

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,9 +4,9 @@ export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
     <div className="Phonetic">
-      {/* <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
-      </a> */}
+      </a>
       <br />
       {props.phonetic.text}
     </div>
