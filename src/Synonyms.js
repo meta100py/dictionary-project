0451b2a28c1da29ac93666f7489a1bfcc5b6bcda
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2025-07-02 17:13:22 -0300

    Styled the application

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,9 +1,7 @@
 import React from "react";
-
+import "./Synonyms.css";
 export default function Synonyms(props) {
-  //   console.log(props.synonyms);
   if (props.synonyms) {
-    // return props.synonyms;
     return (
       <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
